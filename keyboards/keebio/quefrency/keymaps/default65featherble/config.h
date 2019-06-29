a7d0c04fe3765a372f6255dbc05cc9c160ddb290
Sam McCagg <sam.mccagg@ellucian.com>
2019-06-29 00:11:04 -0400

    Add default65featherble.

---
+++ keyboards/keebio/quefrency/keymaps/default65featherble/config.h
@@ -0,0 +1,40 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+Copyright 2018 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Use I2C or Serial, not both */
+
+#define USE_SERIAL
+// #define USE_I2C
+
+#undef DESCRIPTION
+#define DESCRIPTION     65% Quefrency with BLE
+
+#undef MATRIX_ROW_PINS
+#undef MATRIX_COL_PINS
+#define MATRIX_ROW_PINS { D6, F4, F0, D2, D3, F1 }
+#define MATRIX_COL_PINS { B7, B6, B5, D7, C6, D0, D1, F7 }
+#undef UNUSED_PINS
+#define UNUSED_PINS {B5}
+
+
+#define DIODE_DIRECTION ROW2COL

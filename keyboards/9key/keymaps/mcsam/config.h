c78c7a44791511692b81e4a7f8ec4652817e8ef7
Sam McCagg <samuel.mccagg@gmail.com>
2019-08-17 14:03:35 -0400

    re-do with pro micro.

--- keyboards/9key/keymaps/mcsam/config.h
+++ keyboards/9key/keymaps/mcsam/config.h
@@ -3,10 +3,15 @@
 #undef DESCRIPTION
 #define DESCRIPTION     Handwired 9key macro pad
 
+#undef MATRIX_ROW_PINS
+#define MATRIX_ROW_PINS { E6, B4, B5 }
 #undef MATRIX_COL_PINS
-#define MATRIX_COL_PINS { F6, F5, F4 }
+#define MATRIX_COL_PINS { B3, B2, B6 }
+
+#undef RGB_DI_PIN
+#define RGB_DI_PIN B1
 
 #undef RGBLED_NUM
-#define RGBLED_NUM 4    // Number of LEDs
+#define RGBLED_NUM 8    // Number of LEDs3
 
 #define RGBLIGHT_ANIMATIONS
